d8e06d8a283db61c999e039883b7bec7d1b3b516
Jeremiah Ang <jeremiahang94@gmail.com>
2023-06-15 10:00:23 +0000

    Remove extra test

--- __tests__/RockPaperScissors.test.js
+++ __tests__/RockPaperScissors.test.js
@@ -67,38 +67,3 @@ test('Should render records on press of buttons', async () => {
     const records = container.querySelectorAll('ol li');
     expect(records.length).toEqual(4);
 });
-
-test('Should render records on manual addition', async () => {
-    const { container } = render(<RockPaperScissors />);
-
-    const selectElement = container.querySelector('select');
-    const addButton = selectElement.parentElement.querySelector('button');
-
-    await userEvent.selectOptions(selectElement, ['Rock']);
-    await userEvent.click(addButton);
-
-    await userEvent.selectOptions(selectElement, ['Paper']);
-    await userEvent.click(addButton);
-
-    await userEvent.selectOptions(selectElement, ['Scissors']);
-    await userEvent.click(addButton);
-
-    await userEvent.click(container.querySelector('input'));
-
-    await userEvent.selectOptions(selectElement, ['Rock']);
-    await userEvent.click(addButton);
-
-    await userEvent.selectOptions(selectElement, ['Paper']);
-    await userEvent.click(addButton);
-
-    await userEvent.selectOptions(selectElement, ['Scissors']);
-    await userEvent.click(addButton);
-
-    await userEvent.selectOptions(selectElement, ['Paper']);
-    await userEvent.click(addButton);
-
-    await userEvent.selectOptions(selectElement, ['Rock']);
-    await userEvent.click(addButton);
-
-    expect(await screen.findByText(/62\.5/)).toBeInTheDocument();
-});
